8d389312141a977bb6e883789ca645cbad48cce8
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2023-09-05 18:15:39 -0400

    Updated

--- src/test/java/com/jpa/repository/CourseRepositoryTest.java
+++ src/test/java/com/jpa/repository/CourseRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.jpa.repository;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +11,22 @@ import com.jpa.entity.Course;
 
 @SpringBootTest
 public class CourseRepositoryTest {
-    @Autowired
-    private CourseRepository courseRepository;
+  @Autowired
+  private CourseRepository courseRepository;
 
-    @Test
-    public void testSaveCourse() {
-        Course c1 = Course.builder().name("Spring Boot").build();
-        Course c2 = Course.builder().name("Microservices").build();
-        Course c3 = Course.builder().name("RestAPI").build();
-        Course c4 = Course.builder().name("Docker").build();
-        Course c5 = Course.builder().name("K8s").build();
-        courseRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));
-    }
+  @Test
+  public void testSaveCourse() {
+    Course c1 = Course.builder().name("Spring Boot").build();
+    Course c2 = Course.builder().name("Microservices").build();
+    Course c3 = Course.builder().name("RestAPI").build();
+    Course c4 = Course.builder().name("Docker").build();
+    Course c5 = Course.builder().name("K8s").build();
+    courseRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));
+  }
 
-    @Test
-    public void deleteCourseById() {
-        courseRepository.delete(courseRepository.findById(256L).get());
-    }
+  @Test
+  public void deleteCourseById() {
+    Optional<Course> courseToDelete = courseRepository.findById(256L);
+    courseToDelete.ifPresent(course -> courseRepository.delete(course));
+  }
 }
